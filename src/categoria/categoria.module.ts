8854ef06fb57a387f245c9eaf9e67c2b62af724a
Phcode007 <paulobelarmino047@gmail.com>
2025-10-07 18:10:51 -0300

    Implementação do módulo, controller e service

--- src/categoria/categoria.module.ts
+++ src/categoria/categoria.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Categoria } from './entities/categoria.entity';
+import { CategoriaService } from './services/categoria.service';
+import { CategoriaController } from './controller/categoria.controller';
 @Module({
   imports: [TypeOrmModule.forFeature([Categoria])],
-  providers: [],
-  controllers: [],
+  providers: [CategoriaService],
+  controllers: [CategoriaController],
   exports: [],
 })
 export class CategoriaModule {}
---
+++ src/categoria/controller/categoria.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { Categoria } from '../entities/categoria.entity';
+import { CategoriaService } from '../services/categoria.service';
+
+@Controller('/categoria')
+export class CategoriaController {
+  constructor(private readonly categoriaService: CategoriaService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll(): Promise<Categoria[]> {
+    return this.categoriaService.findAll();
+  }
+}
